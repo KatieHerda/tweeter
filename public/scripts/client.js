061118bac15f029b55d5da2894ffbd7f7ffb5866
Katie <katie.herda2@gmail.com>
2021-10-07 03:49:04 +0000

    Moved function call to loadTweets (all) to the bottom of doument.ready function. Added an additional function to load a single tweet when submit button pressed. Added additional comments for clarity.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,61 +7,68 @@
 //
 
 $(document).ready(function() {
-  
+  //Function to render tweets into tweet list
   function renderTweets(array) {
     array.forEach(element => {
-      console.log(element)
+      console.log(element);
       $(`#tweet-list`).prepend(createTweetElement(element));
     });
-  };
+  }
 
 
-//Client side JavaScript
-//takes in tweet object and returns a tweet <article> element containing the entire HTML structure of the tweet.
-const createTweetElement = function(obj) {
+  //Client side JavaScript
+  //takes in tweet object and returns a tweet <article> element containing the entire HTML structure of the tweet.
+  function createTweetElement(obj) {
   //create individual variables that we will use to build tweet
   //header
-  const $tweet = $(`<article class="tweet-container"></article>`);
-  const $header = $(`<header></header>`)
-  const $avatarName = $(`<p><img src="${obj.user.avatars}">${obj.user.name}</p>`);
-  const $handle = $(`<p>${obj.user.handle}</p>`);
+    const $tweet = $(`<article class="tweet-container"></article>`);
+    const $header = $(`<header></header>`);
+    const $avatarName = $(`<p><img src="${obj.user.avatars}">${obj.user.name}</p>`);
+    const $handle = $(`<p>${obj.user.handle}</p>`);
 
-  //main
-  const $main = $(`<div class="text-content"><p></p></div>`)
-  const $text = $(`<p>${obj.content.text}</p>`);
+    //main
+    const $main = $(`<div class="text-content"><p></p></div>`);
+    const $text = $(`<p>${obj.content.text}</p>`);
 
-  //footer
-  const $footer = $(`<footer></footer>`);
-  const $timeCreated = $(`<p>${timeago.format(obj["created_at"])}</p>`);
-  const $otherAvatars = $(`<div><i class="fas fa-flag"></i><i class="fas fa-heart"></i><i class="fas fa-retweet"></i></div>`)
+    //footer
+    const $footer = $(`<footer></footer>`);
+    const $timeCreated = $(`<p>${timeago.format(obj["created_at"])}</p>`);
+    const $otherAvatars = $(`<div><i class="fas fa-flag"></i><i class="fas fa-heart"></i><i class="fas fa-retweet"></i></div>`);
 
-  //build tweet header
-  $header.append($avatarName);
-  $header.append($handle);
+    //build tweet header
+    $header.append($avatarName);
+    $header.append($handle);
   
-  //build tweet main
-  $main.append($text);
+    //build tweet main
+    $main.append($text);
 
-  //build tweet footer
-  $footer.append($timeCreated);
-  $footer.append($otherAvatars);
-  
-  //need to grab flag, heart, retweet from font awesome
+    //build tweet footer
+    $footer.append($timeCreated);
+    $footer.append($otherAvatars);
 
-  //complete build by appending header, footer, and main to $tweet
-  $tweet.append($header);
-  $tweet.append($main);
-  $tweet.append($footer);
+    //complete build by appending header, footer, and main to $tweet
+    $tweet.append($header);
+    $tweet.append($main);
+    $tweet.append($footer);
 
-  return $tweet;
-};
+    return $tweet;
+  }
 
-function loadTweets() {
-  $.ajax('/tweets', { method: 'GET'})
-    .then(function (res) {
-      renderTweets(res);
-     });
-}
+  //loads all tweets on page load
+  function loadTweets() {
+    $.ajax('/tweets', { method: 'GET'})
+      .then(function(res) {
+        renderTweets(res);
+      });
+  }
+
+  //loads one tweet when submit button pressed
+  function loadTweet() {
+    $.ajax('/tweets', { method: 'GET'})
+      .then(function(res) {
+        renderTweets([res[res.length - 1]]);
+      });
+  }
 
   //add event listener for sumbit event
   $("#form").on("submit", function(event) {
@@ -72,13 +79,13 @@ function loadTweets() {
     //serialize tweet data
     const tweet = $(`#form`).serialize();
 
-    console.log(tweet)
+    console.log(tweet);
     //Error if tweet content too long / empty - if so, form should not be cleared
-    //add 5 characters from 'text='
+    //add 5 characters from 'text:'
     if (tweet.length > 145) {
-      alert('Please do not exceed 140 characters per tweet.')
+      alert('Please do not exceed 140 characters per tweet.');
     } else if (tweet === 'text:') {
-      alert('Please enter a tweet before hitting tweet button.')
+      alert('Please enter a tweet before hitting tweet button.');
     } else {
       //happy path: proceed AJAX POST request that sends form data to server
       $.ajax({
@@ -86,14 +93,16 @@ function loadTweets() {
         method: "POST",
         data: tweet
       })
-      .then((res) => {
-        loadTweets();
-        let tweetForm = document.getElementById('tweet-text');
-        tweetForm.value = '';
-      })
-      .catch((err) => {
-        alert('there was an error', err)
-      });
+        .then((res) => {
+          loadTweet();
+          let tweetForm = document.getElementById('tweet-text');
+          tweetForm.value = '';
+        })
+        .catch((err) => {
+          alert('there was an error', err);
+        });
     }
-  })
+  });
+  //Load tweets upon page load
+  loadTweets();
 });
